5a00ee71e510bcb7a998aaaa5021ddbb6afa2d97
fernandocgomez <fernandocgomez@live.com>
2020-02-10 19:14:18 -0600

    kata done

---
+++ javaScript/Sum of angles.js
@@ -0,0 +1,9 @@
+function angle(n) {
+    return (n-2) * 180
+}
+
+console.log(angle(3)) //, 180
+console.log(angle(4)) //, 360
+
+
+// how to get the sum of internal angules of a polygon:  (n-2) X 180
