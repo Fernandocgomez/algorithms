edd4aa3c1fdc9d751207b016025d45ac0268d575
fernandocgomez <fernandocgomez@live.com>
2020-02-10 18:22:11 -0600

    kata done

---
+++ javaScript/Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,14 @@
+var quote = function(fighter) {
+    let george = 'george saint pierre'
+    if( fighter.toLowerCase() == george ) {
+        return "I am not impressed by your performance."
+    }else{
+        return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    }
+};
+
+
+console.log(quote('george saint pierre'))  // "I am not impressed by your performance."
+console.log(quote('conor mcgregor')) // "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+console.log(quote('George Saint Pierre')) // "I am not impressed by your performance."
+console.log(quote('Conor McGregor')) //  "I'd like to take this chance to apologize.. To absolutely NOBODY!"
