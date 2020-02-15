3e7c1950c19a09eb0965adb290302d2a31bd7f68
fernandocgomez <fernandocgomez@live.com>
2020-02-15 15:34:53 -0600

    kata is done

---
+++ ruby/Looking for a benefactor.rb
@@ -0,0 +1,10 @@
+def new_avg(arr, newavg)
+    sum = arr.sum
+    result = (newavg.to_f * (arr.size + 1) - sum).ceil
+    result.negative? ? (raise 'invalid_argument') : result
+end
+
+
+p new_avg([14, 30, 5, 7, 9, 11, 16], 90) # => 628
+p new_avg([14, 30, 5, 7, 9, 11, 15], 92) # => 645
+p new_avg([14, 30, 5, 7, 9, 11, 15], 2) # => ?
