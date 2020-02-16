b429f61c937f6af8f7fba9a10cc3cf6145d6a99f
fernandocgomez <fernandocgomez@live.com>
2020-02-15 20:00:59 -0600

    kata is done

---
+++ javaScript/Counting Duplicates.js
@@ -0,0 +1,17 @@
+function duplicateCount(text){
+    return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+    
+}
+
+console.log(duplicateCount("")) // 0
+console.log(duplicateCount("abcde")) // 0 
+console.log(duplicateCount("a3abbcd3e")) // 2 
+console.log(duplicateCount("aabBcde")) // 2 
+console.log(duplicateCount("Indivisibility")) // 1
+console.log(duplicateCount("Indivisibilities")) // 2 
+
+// split them and get an array with all the chracters
+// check the repeted ones and count them
+// return the n of repeted characters
+
+// /([^])\1+/g  chracters that repete but that are toghter 
